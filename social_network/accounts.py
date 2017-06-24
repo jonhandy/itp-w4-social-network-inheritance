b8f788fa947ac33056f997cbcdf6cb3f7e062c6c
Ping Chen <pxx_chen@yahoo.com>
2017-06-24 16:30:07 +0000

    last intro rmotr project social network done! Jon and Ping

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,25 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
+    
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        post_list = []
+        for user in self.following:
+            post_list += user.posts
+        
+        post_list.sort(key=lambda x: x.timestamp, reverse=True)
+        return post_list
+
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+        
